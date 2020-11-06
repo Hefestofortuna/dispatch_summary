8b52cfa632e3efc1ba219f602b1269b715355da3
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2020-11-06 10:49:16 +0800

    One more commit for fix "No module named ***"

--- dispatch_summary/urls.py
+++ dispatch_summary/urls.py
@@ -1,4 +1,4 @@
-"""Dispatch_Summary URL Configuration
+"""dispatch_summary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- dispatch_summary/wsgi.py
+++ dispatch_summary/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Dispatch_Summary project.
+WSGI config for dispatch_summary project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dispatch_Summary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dispatch_summary.settings')
 
 application = get_wsgi_application()
