f46a26b87d4b6a6728efbf20fc347032d6bd93ae
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2020-11-10 23:42:20 +0800

    JournalNotice
    Added JournalNotice model. I thing without bugs. Fuck,  why i type this is here?

--- journals/admin.py
+++ journals/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import JournalContractor
+from .models import JournalContractor, JournalNotice
 
 
 class JournalContractorAdmin(admin.ModelAdmin):
@@ -9,4 +9,14 @@ class JournalContractorAdmin(admin.ModelAdmin):
     filter_horizontal = ('journal_contractor_file',)
 
 
+class JournalNoticeAdmin(admin.ModelAdmin):
+    list_display = ('journal_notice_date_plan', 'journal_notice_create_user', 'journal_notice_time_start',
+                    'journal_notice_time_end', 'journal_notice_subdivision', 'journal_notice_place',
+                    'journal_notice_description', 'journal_notice_date_of_entry_in_arm_lp',
+                    'journal_notice_translate_user', 'journal_notice_number_acceptance',
+                    'journal_notice_fact_delivery_date',)
+    list_display_links = ('journal_notice_number_acceptance',)
+
+
 admin.site.register(JournalContractor, JournalContractorAdmin)
+admin.site.register(JournalNotice, JournalNoticeAdmin)
--- journals/models.py
+++ journals/models.py
@@ -47,3 +47,31 @@ class JournalContractor(models.Model):
     class Meta:
         verbose_name = 'Работа подрядной организации'
         verbose_name_plural = 'Работа подрядных организаций'
+
+
+class JournalNotice(models.Model):
+    journal_notice_date_plan = models.DateTimeField(_('Дата выполенния работ'), null=False, blank=False)
+    journal_notice_time_start = models.TimeField(_('Начальное время по МСК'), null=False, blank=False)
+    journal_notice_time_end = models.TimeField(_('Конечное время по МСК'), null=False, blank=False)
+    journal_notice_subdivision = models.ForeignKey(subdivisions.models.Subdivision, on_delete=models.SET_NULL,
+                                                   null=True, blank=False, verbose_name='Место на предупреждение')
+    journal_notice_description = models.TextField(_('Описание'), null=False, blank=False)
+    journal_notice_place = models.CharField(_('Место работ (№ пути, километр, пикет)'), max_length=128, null=False,
+                                            blank=False)
+    journal_notice_date_of_entry_in_arm_lp = models.DateTimeField(_('Дата и время внесения в АРМ-ЛП по МСК'), null=True,
+                                                                  blank=True)
+    journal_notice_create_user = models.ForeignKey(users.models.User, on_delete=models.SET_NULL, null=True, blank=False,
+                                                   verbose_name='Пользователь')
+    journal_notice_translate_user = models.ForeignKey(users.models.User, on_delete=models.SET_NULL, null=True,
+                                                      blank=False, verbose_name='Пользователь передавший в АРМ-"ЛП"',
+                                                      related_name='+')
+    journal_notice_number_acceptance = models.IntegerField(_('Номер принятия'), unique=True, null=False, blank=True)
+    journal_notice_fact_delivery_date = models.DateTimeField(_('Время фактического конца работ'), auto_now_add=True)
+    journal_notice_pub_date = models.DateTimeField(_('Время публикации'), auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.journal_notice_subdivision, self.journal_notice_number_acceptance)
+
+    class Meta:
+        verbose_name = 'Предупреждение на производство работы'
+        verbose_name_plural = 'Предупреждения на производство работ'
