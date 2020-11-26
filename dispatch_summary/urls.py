beabd5ac3d41a2d410b69d6d6c9dcf318a114a28
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2020-11-26 23:45:31 +0800

    Update datepicker CSS

--- dispatch_summary/urls.py
+++ dispatch_summary/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +25,6 @@ urlpatterns = [
     path('feed/', include('feeds.urls')),
     path('main/', include('main.urls')),
     path('journals/', include('journals.urls')),
+    path('jsi18n', JavaScriptCatalog.as_view(), name='js-catlog'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- journals/forms.py
+++ journals/forms.py
@@ -4,6 +4,7 @@ from .models import JournalFactoryOfWork
 
 class JournalFactortOfWorkForm(ModelForm):
     journal_factory_of_work_date_start = DateField(widget=AdminSplitDateTime())
+    journal_factory_of_work_date_finish = DateField(widget=AdminSplitDateTime())
     class Meta:
         model = JournalFactoryOfWork
         fields = '__all__'
