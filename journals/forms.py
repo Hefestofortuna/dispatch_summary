7a84f6a0b3722c50eb61d128450cb51c3968d402
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2020-11-30 20:17:15 +0800

    OMG , WOW!
    self.*..initial = user.pk

--- journals/forms.py
+++ journals/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.admin.widgets import AdminSplitDateTime
 from django.forms import ModelForm, DateField, Textarea, Select, ModelChoiceField
 from django.utils.translation import ugettext_lazy as _
-
 from users.models import User
 from .models import JournalFactoryOfWork
 
@@ -11,13 +10,14 @@ class JournalFactoryOfWorkForm(ModelForm):
                                                    label=_('Начало периода'))
     journal_factory_of_work_date_finish = DateField(widget=AdminSplitDateTime(),
                                                     label=_('Конец периода'))
-    journal_factory_of_work_user = ModelChoiceField(queryset=User.objects.filter(groups__name='ШН'),
-                                                    label=_('Пользователь'))
+    journal_factory_of_work_user = ModelChoiceField(queryset = User.objects.filter(groups__name='ШН'),
+                                                    label=_('Пользователь'),
+                                                    empty_label=None)
     journal_factory_of_work_user.widget.attrs.update({'class': 'uk-select', })
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, user, *args, **kwargs):
         super(JournalFactoryOfWorkForm, self).__init__(*args, **kwargs)
-        self.fields['journal_factory_of_work_user'].empty_label = None
+        self.fields['journal_factory_of_work_user'].initial = user.pk
 
     class Meta:
         model = JournalFactoryOfWork
--- journals/views.py
+++ journals/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .forms import JournalFactoryOfWorkForm
+from users.models import User
 
 
 def JournalFactoryOfWorkCreateView(request):
-    form = JournalFactoryOfWorkForm(initial={'supervisor':request.user})
+    form = JournalFactoryOfWorkForm(user=request.user)
     return render(request, 'JournalFactoryOfWork/create.html', {'form': form})
