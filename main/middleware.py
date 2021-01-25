af5bd0f76c61622ad83039296a0f0b5de6d5bd83
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2021-01-26 00:16:01 +0800

    Datetime in cookies

--- main/middleware.py
+++ main/middleware.py
@@ -14,3 +14,16 @@ class AuthRequiredMiddleware(MiddlewareMixin):
         if not request.user.is_authenticated and resolve(request.path).url_name != 'login':
             return redirect('/main/login/')
         return response
+
+
+class CookiesMiddleware(MiddlewareMixin):
+
+    def __init__(self, get_response):
+        super().__init__(get_response)
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if not request.user.is_authenticated and resolve(request.path).url_name != 'login':
+            return redirect('/main/login/')
+        return response
