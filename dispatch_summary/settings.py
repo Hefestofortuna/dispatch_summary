5f5d43386f3b9b957cdc254855bf44ba90b5509d
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2021-02-11 01:19:52 +0800

    FOR ALL

--- dispatch_summary/settings.py
+++ dispatch_summary/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '8l68xnp#qsyr(m5f@+3#iwm%8u_e(l%w_pw0q5u^)tz2%e%t=g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
