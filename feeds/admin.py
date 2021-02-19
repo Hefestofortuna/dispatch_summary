04186761cd537c6913dd06dede1032373994a736
Hefestofortuna <56866681+Hefestofortuna@users.noreply.github.com>
2021-02-19 08:12:54 +0800

    last today

--- feeds/admin.py
+++ feeds/admin.py
@@ -6,10 +6,16 @@ from .forms import FeedForm
 class FeedAdmin(admin.ModelAdmin):
     list_display = ('feed_title', 'feed_text', 'feed_pub_date', 'feed_author', 'get_tags')
     list_display_links = ('feed_title',)
+    list_filter = ('feed_title', 'feed_text', 'feed_pub_date', 'feed_author',)
     filter_horizontal = ('feed_tags', 'feed_file',)
     fields = ['feed_title', 'feed_text', 'feed_file', 'feed_tags', ]
     form = FeedForm
     icon_name = 'chat_bubble_outline'
+
+    def save_model(self, request, obj, form, change):
+        super(FeedAdmin, self).save_model(request, obj, form, change)
+        obj.feed_author = request.user
+        obj.save()
 
 
 class TagAdmin(admin.ModelAdmin):
--- feeds/forms.py
+++ feeds/forms.py
@@ -8,10 +8,7 @@ from django.contrib.admin import ModelAdmin
 
 class FeedForm(ModelForm):
 
-    def __init__(self, user, *args, **kwargs):
-        super(FeedForm, self).__init__(*args, **kwargs)
-        self.fields['feed_text'].label = "Содержание новости"
 
     class Meta:
         model = Feed
-        fields = ['feed_title', 'feed_text', 'feed_file', 'feed_tags', ]
+        fields = '__all__'
